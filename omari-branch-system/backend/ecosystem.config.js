e2d2fdd73a49e8bd663e6ef0a6a93bdec38b0b93
Lourence Ruhwaya <162551120+Anotida28@users.noreply.github.com>
2026-06-16 15:50:05 +0200

    Delete unnecessary log files and update ecosystem configuration for improved process management

--- omari-branch-system/backend/ecosystem.config.js
+++ omari-branch-system/backend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "omari-branch-system",
       script: "dist/server.js",
       cwd: __dirname,
+      exec_mode: "fork",
       instances: 1,
       autorestart: true,
       watch: false,
